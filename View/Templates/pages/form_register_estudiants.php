499ba8e906bf97d40737e0c4d9130a335ca1a20e
Cristiandavid3d@gmail.com <Cristian>
2025-11-25 20:36:55 -0500

    second commit

---
+++ View/Templates/pages/form_register_estudiants.php
@@ -0,0 +1,11 @@
+<?php include "../Templates/header.php" ?>
+
+<h1>Registro estudiantes</h1>
+
+<div class="row">
+    <div class="col">
+
+    </div>
+</div>
+
+<?php include "../Templates/footer.php" ?>
