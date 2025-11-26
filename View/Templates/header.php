85748fab265550d82cc3d99c14a7d4bfd2545828
Cristiandavid3d@gmail.com <Cristian>
2025-11-25 20:47:25 -0500

    fix register

--- View/Templates/header.php
+++ View/Templates/header.php
@@ -25,7 +25,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/pages/form_register_estudiants.php">Registro
+                        <a class="nav-link active" aria-current="page" href="../pages/form_register_estudiants.php">Registro
                             estudiantes</a>
                     </li>
                     <li class="nav-item">
